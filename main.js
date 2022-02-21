ebb61f8ec167dfdfe6010f57e9617e6399828968
ta211 <55717622+ta211@users.noreply.github.com>
2022-02-21 10:55:46 -0500

    Complete item (included for newly added items)

--- main.js
+++ main.js
@@ -9,13 +9,27 @@ function addListItem() {
     let newItem = document.createElement("li");
     // Item text
     let itemInput = document.getElementById("text-input");
-    newItem.appendChild(document.createTextNode(itemInput.value));
-    // Delete button
+    // Create checkbox 
+    // <label for="check-value">Value</label>
+    let checkId = "check-"+String(itemInput.value).toLowerCase();
+    let checkLabel = document.createElement("label");
+    checkLabel.appendChild(document.createTextNode(itemInput.value));
+    checkLabel.setAttribute("for", checkId);
+    // <input id="check-value" type="checkbox" class="complete-item" onclick="completeListItem(this)">
+    let checkInput = document.createElement("input");
+    checkInput.setAttribute("id", checkId);
+    checkInput.setAttribute("type", "checkbox");
+    checkInput.setAttribute("class", "complete-item");
+    checkInput.setAttribute("onclick", "completeListItem(this)");
+    // Create delete button
     let newButton = document.createElement("button");
     newButton.appendChild(document.createTextNode("X"));
     newButton.setAttribute("class","delete-item");
     newButton.setAttribute("onclick", "deleteListItem(this.parentNode)");
     // Add to list item
+    // newItem.appendChild(document.createTextNode(itemInput.value));
+    newItem.appendChild(checkLabel);
+    newItem.appendChild(checkInput);
     newItem.appendChild(newButton);
     // Add to list
     list.appendChild(newItem);
@@ -23,4 +37,14 @@ function addListItem() {
 
 function deleteListItem(item) {
     item.parentNode.removeChild(item);
+}
+
+function completeListItem(item) {
+    if (item.checked) { // true if the input checkbox is checked
+        item.parentNode.style.textDecoration = "line-through";
+        // in css, this would be: "text-decoration: line-through"
+    } else {
+        item.parentNode.style.textDecoration = "none";
+        // in css, this would be: "text-decoration: none"
+    }
 }
