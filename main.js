4f8acee75b26084b4dfd850a96e4df58f88d9f50
ta211 <55717622+ta211@users.noreply.github.com>
2022-02-21 10:47:06 -0500

    Add list item and delete list item

--- main.js
+++ main.js
@@ -2,3 +2,25 @@ function addNewList() {
     alert('hello world alert!');
     console.log('hello world console');
 }
+
+function addListItem() {
+    let list = document.getElementById("grocery-list");
+    // Create parent list item
+    let newItem = document.createElement("li");
+    // Item text
+    let itemInput = document.getElementById("text-input");
+    newItem.appendChild(document.createTextNode(itemInput.value));
+    // Delete button
+    let newButton = document.createElement("button");
+    newButton.appendChild(document.createTextNode("X"));
+    newButton.setAttribute("class","delete-item");
+    newButton.setAttribute("onclick", "deleteListItem(this.parentNode)");
+    // Add to list item
+    newItem.appendChild(newButton);
+    // Add to list
+    list.appendChild(newItem);
+}
+
+function deleteListItem(item) {
+    item.parentNode.removeChild(item);
+}
